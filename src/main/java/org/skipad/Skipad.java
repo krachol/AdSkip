85ce7c16e4a944a8409fcaf87ada709e4cf322cb
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2017-04-09 21:13:44 +0200

    Add files that github gui skipped previously

---
+++ src/main/java/org/skipad/Skipad.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Mateusz Krasicki on 09.04.2017.
+ */
+
+package org.skipad;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class Skipad {
+    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello AdSkip!! ");
+        return "hello";
+    }
+}
