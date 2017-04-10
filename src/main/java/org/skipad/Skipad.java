5dfdc02fd10bcb9d3565b0141497cff790c2d05e
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2017-04-11 00:48:12 +0200

    Add javadoc

--- src/main/java/org/skipad/Skipad.java
+++ src/main/java/org/skipad/Skipad.java
@@ -4,14 +4,24 @@
 
 package org.skipad;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+/**
+ * Controller for / path
+ */
 @Controller
 @RequestMapping("/")
 public class Skipad {
+    /**
+     * Main method invoked when server receives GET request on /
+     * @param model model for jsp view
+     * @return name of the view to return to the client
+     */
     @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello AdSkip!");
         return "hello";
