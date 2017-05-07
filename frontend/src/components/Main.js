56ca0d86a5ec8a5c9b1d2571214da34ba3a8c5f3
Tomasz Kasperek <kasperekt@gmail.com>
2017-05-07 13:50:21 +0200

    Channel list

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-import Button from 'ui/Button';
-import style from './Main.scss';
+import Header from 'layout/Header';
+import Content from 'layout/Content';
+import ChannelList from './ChannelList';
 
 export default class Main extends Component {
   render() {
     return (
       <div>
-        <h1 className={style['Main__heading']}>Ad Skip</h1>
-        <main>
-          Running on <a href="https://reactjs.org">React.js</a>
-          <Button>Hello</Button>
-        </main>
+        <Header />
+        <Content>
+          <ChannelList />
+        </Content>
       </div>
     );
   }
---
+++ frontend/src/ui/Icon.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Icon({ type }) {
+  return <svg />;
+}
