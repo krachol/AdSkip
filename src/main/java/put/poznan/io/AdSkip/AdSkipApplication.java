c14091244ab19f5efeb74d22e5f229c191b3abb3
Jacek Kubiak <jk.kubiak@gmail.com>
2017-05-15 15:39:02 +0200

    Moved our project under spring boot

---
+++ src/main/java/put/poznan/io/AdSkip/AdSkipApplication.java
@@ -0,0 +1,12 @@
+package put.poznan.io.AdSkip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSkipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdSkipApplication.class, args);
+	}
+}
---
+++ src/test/java/put/poznan/io/AdSkip/AdSkipApplicationTests.java
@@ -0,0 +1,16 @@
+package put.poznan.io.AdSkip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdSkipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
